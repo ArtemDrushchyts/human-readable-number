958c39d1be960f43fd1d1675a4906007eed51194
Artem Drushchyts <temafm-3@yandex.ru>
2020-02-23 18:13:58 +0300

    feat: complete task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
-  
+  let arr = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 
+            'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let arr2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    let num = number;
+  let str = '';
+  if(num == 0) {
+      str = arr[0];
+  }
+  if(Math.trunc(number/100) != 0) {
+      str += arr[Math.trunc(number/100)] + ' hundred ';
+      num -= Math.trunc(number/100)*100; 
+  }
+  if(Math.trunc(num/10) != 0) {
+    if( Math.trunc(num) > 19) {
+    str += arr2[Math.trunc(num/10) - 2] + ' ';
+    num -= Math.trunc(num/10)*10;
+    } else {
+        str += arr[Math.trunc(num)] + ' ';
+        num -= Math.trunc(num);
+    }
+ }
+if(num < 10 && num > 0) {
+    str += arr[num];
+    
+}
+ 
+
+  return str.trim();
 }
